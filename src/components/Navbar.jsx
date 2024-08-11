c6e8e6b3083c9c6e61d554b54309a510b6a8b970
harrykamboj1 <singhharnoor116@gmail.com>
2024-08-11 21:05:32 +0530

    Navbar changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,110 +4,40 @@ import { Bio } from "../data/data";
 import { Menu } from "lucide-react";
 
 const navItemClass = `text-customDarkPurple hover:scale-105 duration-60 font-medium cursor-pointer no-underline transition-all ease-in-out hover:text-[#8f64c3]`;
+const menuNavItemClass = `text-darkBlue hover:scale-105 duration-60 font-medium cursor-pointer no-underline transition-all ease-in-out hover:text-[#8f64c3]`;
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
-  const [isHidden, setIsHidden] = useState(false);
-  let lastScrollTop = 0; // To keep track of the last scroll position
-
-  useEffect(() => {
-    const handleScroll = () => {
-      const currentScrollTop =
-        window.pageYOffset || document.documentElement.scrollTop;
-
-      if (currentScrollTop > lastScrollTop && currentScrollTop > 50) {
-        // Scrolling down
-        setIsHidden(true);
-      } else {
-        // Scrolling up
-        setIsHidden(false);
-      }
-      lastScrollTop = currentScrollTop <= 0 ? 0 : currentScrollTop; // For Mobile or negative scrolling
-    };
-
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, [lastScrollTop]);
 
   return (
     <div
-      className={`h-20 flex items-center md:justify-evenly justify-between text-xl md:static sticky top-0 z-10  transition-transform duration-300 ${
-        isHidden ? "-translate-y-full" : "translate-y-0"
-      }`}
+      className={`h-20 flex items-center justify-between text-xl top-0 z-10  transition-transform duration-300
+        `}
     >
       <div className="text-customDarkPurple text-2xl hover:scale-105 duration-60 font-semibold transition-all ease-in-out hover:text-[#8f64c3] no-underline py-1 pl-10">
         <a href="/">Portfolio</a>
       </div>
 
-      <div className="h-full items-center pr-6 md:hidden flex">
-        <Menu
-          onClick={() => setIsOpen(!isOpen)}
-          className="text-customDarkPurple"
-        />
-      </div>
-
-      {isOpen && (
-        <ul
-          className={`w-full ${isOpen ? "translate-y-0" : "-translate-y-25"} ${
-            isOpen ? "opacity-100 " : "opacity-0"
-          } ${
-            isOpen ? "z-1000" : "-z-1000"
-          } gap-[16px] flex justify-start transition-all rounded-[8px] shadow-2xl delay-75 ease-in-out flex-col pt-3 pr-10 pb-6 pl-10 absolute top-[80px] right-0 bg-customDarkPurple md:hidden list-none`}
-        >
-          <a
-            className={`text-[#201f2c] hover:scale-105 duration-60 font-medium cursor-pointer no-underline transition-all ease-in-out hover:text-[#0c0c11] hover:font-semibold`}
-            href="#About"
-          >
+      {/* LINKS */}
+      <div>
+        <ul className="w-full items-center justify-center gap-[34px] sm:flex hidden list-none">
+          <a className={navItemClass} href="#About">
             About
           </a>
-          <a
-            className={`text-[#201f2c] hover:scale-105 duration-60 font-medium cursor-pointer no-underline transition-all ease-in-out hover:text-[#0c0c11] hover:font-semibold`}
-            href="#Skills"
-          >
+          <a className={navItemClass} href="#Skills">
             Skills
           </a>
-          <a
-            className={`text-[#201f2c] hover:scale-105 duration-60 font-medium cursor-pointer no-underline transition-all ease-in-out hover:text-[#0c0c11] hover:font-semibold`}
-            href="#Experience"
-          >
+          <a className={navItemClass} href="#Experience">
             Experience
           </a>
-          <a
-            className={`text-[#201f2c] hover:scale-105 duration-60 font-medium cursor-pointer no-underline transition-all ease-in-out hover:text-[#0c0c11] hover:font-semibold`}
-            href="#Projects"
-          >
+          <a className={navItemClass} href="#Projects">
             Projects
           </a>
-          <a
-            href={Bio.github}
-            target="_blank"
-            rel="noreferrer"
-            className="text-customDarkPurple w-fit flex gap-1 rounded-[20px] border-2 justify-center items-center transition-all ease-out delay-75 duration-75 bg-[#201f2c] cursor-pointer text-[16px] font-semibold no-underline py-2 px-5 hover:bg-[#201f2c] hover:text-[#ffffff] hover:border-r-customDarkPurple hover:scale-110"
-          >
-            <GitHub>Github</GitHub>
-            <div>Github</div>
-          </a>
         </ul>
-      )}
-
-      {/* LINKS */}
-      <ul className="w-full items-center justify-center gap-[34px] md:flex hidden list-none">
-        <a className={navItemClass} href="#About">
-          About
-        </a>
-        <a className={navItemClass} href="#Skills">
-          Skills
-        </a>
-        <a className={navItemClass} href="#Experience">
-          Experience
-        </a>
-        <a className={navItemClass} href="#Projects">
-          Projects
-        </a>
-      </ul>
+      </div>
 
       {/* SOCIAL LINKS */}
-      <div className="h-full items-center justify-center py-0 pr-6 md:flex hidden">
+      <div className="h-full items-center justify-center py-0 pr-6 sm:flex hidden">
         <a
           href={Bio.github}
           target="_blank"
@@ -117,6 +47,42 @@ const Navbar = () => {
           <GitHub>Github</GitHub>
           <div>Github</div>
         </a>
+      </div>
+
+      <div className="sm:hidden flex items-center justify-end">
+        <Menu
+          onClick={() => setIsOpen(!isOpen)}
+          className="text-customDarkPurple"
+        />
+        <div
+          className={`${
+            isOpen ? "flex" : "hidden"
+          } bg-customDarkPurple  opacity-100 overflow-auto z-50 absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl p-6 sidebar`}
+        >
+          <ul className="list-none  flex flex-col justify-end items-center flex-1">
+            <a className={menuNavItemClass} href="#About">
+              About
+            </a>
+            <a className={menuNavItemClass} href="#Skills">
+              Skills
+            </a>
+            <a className={menuNavItemClass} href="#Experience">
+              Experience
+            </a>
+            <a className={menuNavItemClass} href="#Projects">
+              Projects
+            </a>
+            <a
+              href={Bio.github}
+              target="_blank"
+              rel="noreferrer"
+              className="text-customDarkPurple w-fit flex gap-1 rounded-[20px] border-2 justify-center items-center transition-all ease-out delay-75 duration-75 bg-[#201f2c] cursor-pointer text-[16px] font-semibold no-underline py-2 px-5 hover:bg-[#201f2c] hover:text-[#ffffff] hover:border-r-customDarkPurple hover:scale-110"
+            >
+              <GitHub>Github</GitHub>
+              <div>Github</div>
+            </a>
+          </ul>
+        </div>
       </div>
     </div>
   );
