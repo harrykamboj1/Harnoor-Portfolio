34f8c457bd1b238d5faf596995ad037ccdbdc69e
harrykamboj1 <singhharnoor116@gmail.com>
2025-12-06 08:47:36 +0530

    Resume Link change

--- src/data/data.js
+++ src/data/data.js
@@ -7,7 +7,7 @@ export const Bio = {
     github: "https://github.com/harrykamboj1",
     leetCode: "https://leetcode.com/u/singhharnoor116/",
     resume:
-        "https://drive.google.com/file/d/1k-kJ_496kTOfR-8vL9U_BwWEcjLNQhkd/view?usp=drive_link",
+        "https://drive.google.com/file/d/1CAGzp_qGVo5SBz3l5Q9Uho3gKu9SQrRn/view?usp=sharing",
     linkedin: "https://www.linkedin.com/in/harnoor-singh-3b163714b/",
     twitter: "https://twitter.com/Harnoor29581982",
 };
