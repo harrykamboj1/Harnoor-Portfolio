e2ac6987eef6c44c94d9991e8473a902bedc88f3
harrykamboj1 <singhharnoor116@gmail.com>
2026-08-03 21:17:05 +0530

    fix: Update resume link to the correct sharing URL

--- src/data/data.js
+++ src/data/data.js
@@ -40,7 +40,7 @@ export const Bio = {
     github: "https://github.com/harrykamboj1",
     leetCode: "https://leetcode.com/u/singhharnoor116/",
     resume:
-        "https://drive.google.com/file/d/1oEPbc_g1Q8nXSsGt1lhUawLij24NYSnZ/view?usp=sharing",
+        "https://drive.google.com/file/d/1gNxjyoUl5RU2dUTPIPqtaE3G6UGG7W1E/view?usp=drive_link",
     linkedin: "https://www.linkedin.com/in/harnoor-singh-3b163714b/",
     twitter: "https://twitter.com/Harnoor29581982",
 };
